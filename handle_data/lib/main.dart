e4bd145e4d0d67209b916e46e7b8c5768f59d340
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-09-10 14:54:21 +0900

    [Update] web api get and post

--- handle_data/lib/main.dart
+++ handle_data/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:ui' as ui;
 import 'dart:io';
+import 'dart:convert'; // utf8.decoder, utf8.encoder
 
 // ライブラリ(pub spec)
 import 'package:path_provider/path_provider.dart';
@@ -43,6 +44,16 @@ class _HandleDataPageState extends State<HandleDataPage> {
   final _filename = 'flutter_sample_data.txt';
   final _resource_filename = 'assets/documents/test_resource_data.txt';
 
+  // Web Api for GET
+  final _ctrlWebApi = TextEditingController();
+  static const host = 'baconipsum.com';
+  static const path = '/api/?type=meat-and-filter&paras=1&format=text';
+  final port = 80; // 80番ポート (http)
+
+  // Web Api for POST
+  static const postPath = 'https://jsonplaceholder.typicode.com/posts';
+  final portPost = 443; // 443番ポート (https)
+
   // 設定情報からの値を一時保存
   double _r = 0.0;
   double _g = 0.0;
@@ -66,19 +77,18 @@ class _HandleDataPageState extends State<HandleDataPage> {
           children: <Widget>[
             Text(
               'FILE ACCESS.',
-              style: TextStyle(fontSize: 32, fontWeight: ui.FontWeight.w500),
+              style: TextStyle(fontSize: 16, fontWeight: ui.FontWeight.w500),
             ),
             Text(
               "下記に保存したいテキストを記入してください",
             ),
-            Padding(padding: EdgeInsets.all(10.0)),
             TextField(
-                controller: _controller,
-                style: TextStyle(fontSize: 24),
-                minLines: 1,
-                maxLines: 5),
+              controller: _controller,
+              style: TextStyle(fontSize: 24),
+              minLines: 1,
+              maxLines: 5,
+            ),
             Text('設定値で画像のグラデーションを変更する'),
-            Padding(padding: EdgeInsets.all(10.0)),
             Slider(
               min: 0.0,
               max: 255.0,
@@ -118,6 +128,19 @@ class _HandleDataPageState extends State<HandleDataPage> {
               height: 125,
               color: Color.fromARGB(255, _r.toInt(), _g.toInt(), _b.toInt()),
             ),
+            // Web Api
+            Text(
+              'INTERNET ACCESS.',
+              style: TextStyle(fontSize: 16, fontWeight: ui.FontWeight.w500),
+            ),
+            TextField(
+              controller: _ctrlWebApi,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              minLines: 1,
+              maxLines: 5,
+            ),
           ],
         ),
       ),
@@ -129,15 +152,15 @@ class _HandleDataPageState extends State<HandleDataPage> {
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             label: 'Save to Device',
-            icon: Icon(Icons.save, color: Colors.white, size: 32),
+            icon: Icon(Icons.save, color: Colors.white, size: 24),
           ),
           BottomNavigationBarItem(
             label: 'Load from Device',
-            icon: Icon(Icons.open_in_new, color: Colors.white, size: 32),
+            icon: Icon(Icons.open_in_new, color: Colors.white, size: 24),
           ),
           BottomNavigationBarItem(
             label: 'Save to Prefs',
-            icon: Icon(Icons.save, color: Colors.white, size: 32),
+            icon: Icon(Icons.save, color: Colors.white, size: 24),
           ),
         ],
         onTap: (int value) async {
@@ -186,17 +209,52 @@ class _HandleDataPageState extends State<HandleDataPage> {
           }
         },
       ),
+      // アセットからデータを読み込む
+      // floatingActionButton: FloatingActionButton(
+      //     child: Icon(Icons.open_in_new),
+      //     onPressed: () async {
+      //       String text;
+      //       try {
+      //         text = await getDataOfAssetsFromResource(_resource_filename);
+      //       } catch (e) {
+      //         text = "*** no data in assets ***";
+      //       }
+      //       setState(() {
+      //         _controller.text = text;
+      //       }
+      //     );
+      //   }
+      // ),
+      // WebApiからデータを取得する(GET)
+      // floatingActionButton: FloatingActionButton(
+      //     child: Icon(Icons.arrow_circle_down),
+      //     onPressed: () async {
+      //       String text;
+      //       try {
+      //         // Download text throgh Web Api
+      //         text = await getWebApiData();
+      //       } catch (e) {
+      //         text = "*** no data in web api ***";
+      //       }
+      //       setState(() {
+      //         _ctrlWebApi.text = text;
+      //       });
+      //     }
+      // ),
+      // WebApiからデータを取得する(POST)
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.open_in_new),
+          child: Icon(Icons.arrow_circle_down),
           onPressed: () async {
             String text;
             try {
-              text = await getDataOfAssetsFromResource(_resource_filename);
+              // Download text throgh Web Api
+              ResWebApiJson model = await getWebApiPost();
+              text = model.getJsonStr();
             } catch (e) {
-              text = "*** no data in assets ***";
+              text = "*** no data in web api ***";
             }
             setState(() {
-              _controller.text = text;
+              _ctrlWebApi.text = text;
             });
           }),
     );
@@ -256,5 +314,72 @@ class _HandleDataPageState extends State<HandleDataPage> {
       _g = (prefs.getDouble('g') ?? 0.0);
       _b = (prefs.getDouble('b') ?? 0.0);
     });
+  }
+
+  /* WebApiからデータを読み込む */
+  Future<String> getWebApiData() async {
+    var http = await HttpClient();
+    HttpClientRequest request = await http.get(host, port, path);
+    HttpClientResponse response = await request.close();
+    final value =
+        await response.transform(utf8.decoder).join(); // utf-8 -> utf-16
+    return value;
+  }
+
+  /* Web Api サーバーにPOSTして結果を受け取る */
+  Future<ResWebApiJson> getWebApiPost() async {
+    Map<String, dynamic> reqJson = {
+      'title': 'Flutter',
+      'author': 'inoue shinichi',
+      'content': 'Article of Web Api Access'
+    };
+
+    final jsonData = json.encode(reqJson);
+    var http = await HttpClient();
+    // HttpClientRequest request = await http.post(Uri.https(/*host*/, /*path*/, /*query(Map)*/));
+    HttpClientRequest request = await http.postUrl(Uri.parse(postPath));
+    request.headers
+        .set(HttpHeaders.contentTypeHeader, "application/json; charset=UTF-8");
+    request.write(jsonData); // bodyにjson形式の文字列を格納
+    HttpClientResponse response = await request.close(); // POST
+
+    final String jsonStr = await response.transform(utf8.decoder).join();
+    final jsonMap =
+        json.decode(jsonStr) as Map<String, dynamic>; // Json文字列からMapに変換
+    // final String jsonStr = json.encode(jsonMap); // MapからJson文字列に変換
+    return ResWebApiJson.fromJson(jsonMap);
+  }
+}
+
+// サーバーから受信用のJsonModel
+class ResWebApiJson {
+  final int id;
+  final String title;
+  final String author;
+  final String content;
+
+  // コンストラクタ
+  ResWebApiJson(this.id, this.title, this.author, this.content);
+
+  // ファクトリコンストラクタ
+  factory ResWebApiJson.fromJson(Map<String, dynamic> json) {
+    return ResWebApiJson(
+      /* id */ json['id'] as int,
+      /* title */ json['title'] as String,
+      /* author */ json['author'] as String,
+      /* content */ json['content'] as String,
+    );
+  }
+
+  // Json文字列を取得
+  String getJsonStr() {
+    Map<String, dynamic> jsonMap = {
+      "id": id,
+      "title": title,
+      "author": author,
+      "content": content,
+    };
+
+    return json.encode(jsonMap);
   }
 }
